aff3c13101b6f25fb0be2f04eb3a345677f5d3ef
wanghao <1750110604@qq.com>
2019-09-17 11:17:16 +0800

    布局

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Routes from './router'
 import Layout from './pages/Layout'
 
+import 'antd/dist/antd.css'
+
 function App() {
   return (
     <div className="App">
       <Layout>
-        base2
+        <Routes />
       </Layout>
-      base
-      <Routes />
     </div>
   );
 }
