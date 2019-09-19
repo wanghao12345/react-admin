f83e0c067c5b30cec489b19c02e62c9e667d3797
wanghao <1750110604@qq.com>
2019-09-19 18:12:42 +0800

    路由配置

--- src/components/BreadCrumb/index.js
+++ src/components/BreadCrumb/index.js
@@ -5,11 +5,28 @@ import config from '../../router/config'
 function BreadCrumb(props) {
   console.log(props);
 
+  const { location } = props
+  let arr = []
+  Object.keys(config).map(key => {
+    config[key].map(r => {
+      const route = r => {
+        if (r.key === location.pathname ) {
+          arr = r.breadCrumb
+        }
+      }
+      if (r.key === location.pathname ) {
+        arr = r.breadCrumb
+      }
+      return r.component ? route(r) : r.subs.map( r => route(r))
+    })
+  });
+  console.log(arr);
+
   return (
     <Breadcrumb style={{ margin: '16px 0' }}>
-      <Breadcrumb.Item>Home</Breadcrumb.Item>
-      <Breadcrumb.Item>List</Breadcrumb.Item>
-      <Breadcrumb.Item>App</Breadcrumb.Item>
+      {
+        arr.map(r => (<Breadcrumb.Item>{ r }</Breadcrumb.Item>))
+      }
     </Breadcrumb>
   );
 }
