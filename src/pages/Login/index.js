caf4b3ae290fd0db9a66e7bf2a6f57b027936a11
wanghao <1750110604@qq.com>
2019-09-20 11:53:29 +0800

    登录

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,14 +2,84 @@
  * Created by wanghao on 2019/9/14
  */
 import React from 'react';
+import ReactCanvasNest from 'react-canvas-nest'
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import {
+  LoginWrapper,
+  LoginForm
+} from './style'
 
 function Login(props) {
   const { history } = props
+  const { getFieldDecorator } = props.form;
+  const canvasConfig = {
+    pointColor: '169,244,1',
+    lineColor: '255,255,255',
+    count: 200
+  };
+
+  // 登录
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        history.push('/app/home')
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
   return (
-    <div className="LoginWrapper" onClick={() => history.push('/app/home')}>
-      login
-    </div>
+    <LoginWrapper>
+      <ReactCanvasNest
+        className="canvasNest"
+        config={canvasConfig}
+        style={{zIndex: 99}}
+      >
+      </ReactCanvasNest>
+      <LoginForm>
+        <Form onSubmit={handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入用户名!' }],
+            })(
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="用户名"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入密码!' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="密码"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item style={{marginTop: '-15px'}}>
+            {getFieldDecorator('remember', {
+              valuePropName: 'checked',
+              initialValue: true,
+            })(<Checkbox>记住账号</Checkbox>)}
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              style={{width: '100%'}}
+            >
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </LoginForm>
+    </LoginWrapper>
   );
 }
 
-export default Login;
+const WrappedLoginForm = Form.create()(Login);
+export default WrappedLoginForm;
+
---
+++ src/pages/Login/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+
+export const LoginWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  background: rgb(8, 38, 58);
+`;
+export const LoginForm = styled.div`
+    width: 400px;
+    height: 300px;
+    background: #ffffff;
+    position: absolute;
+    z-index: 100;
+    left: 50%;
+    margin-left: -200px;
+    top: 50%;
+    margin-top: -150px;
+    box-sizing: border-box;
+    padding: 50px;
+    border-radius: 5px;
+`;
